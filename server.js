9118eb2470b9a8ea1914c98c56e70f97ec805f60
kiranvsk1 <kiran.vsk1@gmail.com>
2016-02-11 08:03:00 +0530

    Added js libraries

---
+++ server.js
@@ -0,0 +1,14 @@
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname+'/public'));
+
+io.on('connection',function () {
+  console.log("user Connected to socket io");
+});
+http.listen(PORT,function () {
+  console.log('Listening on Port ' +PORT);
+})
